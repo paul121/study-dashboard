d49bad66d7d6b6e07a41bae9d793615267a1364a
Paul Weidner <paul.weidner@gmail.com>
2019-04-30 16:30:01 -0700

    Change icons

--- src/App.js
+++ src/App.js
@@ -5,14 +5,18 @@ import { HenList } from './hens';
 import { CoopList } from './coops';
 import { TreatmentList } from './treatments';
 
+import InvertColorsIcon from '@material-ui/icons/InvertColors';
+import HomeIcon from '@material-ui/icons/Home';
+import PeopleIcon from '@material-ui/icons/People';
+
 import fakeDataProvider from 'ra-data-fakerest';
 
 const dataProvider = fakeDataProvider(require('./data/data.js'));
 const App = () => (
     <Admin dataProvider={dataProvider}>
-        <Resource name="treatments" list={TreatmentList} />
-        <Resource name="coops" list={CoopList} />
-        <Resource name="hens" list={HenList} />
+        <Resource name="treatments" list={TreatmentList} icon={InvertColorsIcon}/>
+        <Resource name="coops" list={CoopList} icon={HomeIcon}/>
+        <Resource name="hens" list={HenList} icon={PeopleIcon}/>
     </Admin>
 );
 
