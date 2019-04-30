a75d9efa0a461fa0281138edc2fb532d5aa28175
Paul Weidner <paul.weidner@gmail.com>
2019-04-30 16:18:18 -0700

    Create fakeDataProvider data file, hens.json

---
+++ src/data/data.js
@@ -0,0 +1,21 @@
+var hens = require('./hens.json');
+
+module.exports = {
+  "treatments": [
+    {"id":1, "treatment-type":"Ferment", "treatment-description":"Fermented feed"},
+    {"id":2, "treatment-type":"Dry", "treatment-description":"Dry feed"},
+    {"id":3, "treatment-type":"Hydrated", "treatment-description":"Hydrated feed"},
+  ],
+  "coops": [
+    {"id":1, "name":"Coop 1", "treatmentID":1, "location":"field"},
+    {"id":2, "name":"Coop 2", "treatmentID":2, "location":"field"},
+    {"id":3, "name":"Coop 3", "treatmentID":3, "location":"field"},
+    {"id":4, "name":"Coop 4", "treatmentID":3, "location":"field"},
+    {"id":5, "name":"Coop 5", "treatmentID":1, "location":"field"},
+    {"id":6, "name":"Coop 6", "treatmentID":2, "location":"field"},
+    {"id":7, "name":"Coop 7", "treatmentID":2, "location":"field"},
+    {"id":8, "name":"Coop 8", "treatmentID":3, "location":"field"},
+    {"id":9, "name":"Coop 9", "treatmentID":1, "location":"field"},
+  ],
+  "hens": hens,
+}
